1d0b9a577fa6e833a79669a28a136faf55b2fc22
franveggiani <francovegginai@gmail.com>
2024-06-02 23:33:31 -0300

    second_commit

---
+++ src/main/java/com/yarmovezzoli/gestioninv/GestioninvApplication.java
@@ -0,0 +1,15 @@
+package com.yarmovezzoli.gestioninv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioninvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioninvApplication.class, args);
+
+		System.out.println("Corriendo aplicación...");
+	}
+
+}
