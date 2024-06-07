f6b2e52573cf7fadf2dc4965f0b97950f7a3a2b6
franveggiani <francovegginai@gmail.com>
2024-06-07 18:52:29 -0300

    Modificacion de buscarPorNombre - Endpoint TipoInventario y EstadoOrdenDeCompra

--- src/main/java/com/yarmovezzoli/gestioninv/Controllers/ModuloArticulos/ProveedorController.java
+++ src/main/java/com/yarmovezzoli/gestioninv/Controllers/ModuloArticulos/ProveedorController.java
@@ -1,7 +1,7 @@
-package com.yarmovezzoli.gestioninv.Controllers;
+package com.yarmovezzoli.gestioninv.Controllers.ModuloArticulos;
 
-import com.yarmovezzoli.gestioninv.Entities.Articulo;
-import com.yarmovezzoli.gestioninv.Services.ArticuloServiceImpl;
+import com.yarmovezzoli.gestioninv.Controllers.BaseControllerImpl;
+import com.yarmovezzoli.gestioninv.Entities.Proveedor;
 import com.yarmovezzoli.gestioninv.Services.ProveedoresServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,19 +10,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "api/v1/articulo")
-public class ArticuloController extends BaseControllerImpl<Articulo, ArticuloServiceImpl>{
+@RequestMapping(path = "api/v1/proveedores")
+
+public class ProveedorController extends BaseControllerImpl<Proveedor,ProveedoresServiceImpl> {
 
     @Autowired
-    ArticuloServiceImpl articuloService;
+    ProveedoresServiceImpl proveedoresService;
     @GetMapping("/buscarPorDenominacion")
     public ResponseEntity<?> buscarPorNombre(@RequestParam String filtroNombre){
         try{
-            return ResponseEntity.status(HttpStatus.OK).body(service.buscarProductoPorNombre(filtroNombre));
+            return ResponseEntity.status(HttpStatus.OK).body(service.buscarPorNombre(filtroNombre));
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"" + e.getMessage() + "\"}");
         }
     }
-
 }
-
