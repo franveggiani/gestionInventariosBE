7fa5c8062e602d4a2f4ad3ac0915524886c3a190
franveggiani <francovegginai@gmail.com>
2024-06-26 12:56:31 -0300

    Proveedor determinado

--- src/main/java/com/yarmovezzoli/gestioninv/Repositories/ProveedorArticuloRepository.java
+++ src/main/java/com/yarmovezzoli/gestioninv/Repositories/ProveedorArticuloRepository.java
@@ -1,6 +1,15 @@
 package com.yarmovezzoli.gestioninv.Repositories;
 
+import com.yarmovezzoli.gestioninv.Entities.Articulo;
 import com.yarmovezzoli.gestioninv.Entities.ProveedorArticulo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProveedorArticuloRepository extends BaseRepository<ProveedorArticulo, Long> {
+
+    @Query("SELECT pa FROM ProveedorArticulo pa WHERE pa.esPredeterminado = :valor AND pa.articulo = :articulo")
+    public List<ProveedorArticulo> findByPredeterminado(@Param("valor") boolean valor, @Param("articulo") Articulo articulo);
+
 }
